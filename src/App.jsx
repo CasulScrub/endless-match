34a92b65f0aa142a8a1846cfbb69898ca95dd665
Joshua Reyes <casulgamestudio@gmail.com>
2024-12-16 12:50:53 -0500

    ✨ Add random calming gradient background
    - 🎨 Implemented simple gradient background component
    - 🌈 Added selection of calming color combinations
    - 🤍 Included subtle white overlay for softness
    - 📐 Proper z-index layering with game content

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import EndlessMatch from './EndlessMatch'
 import { SoundProvider } from './assets/utils/soundManager';
+import GradientBackground from './assets/utils/GradientBackground';
 
 function App() {
   return (
     <SoundProvider>
-    <div className="min-h-screen bg-gray-100 p-4">
-      <EndlessMatch />
-    </div>
+      <div className="relative min-h-screen">
+        <GradientBackground />
+        <div className="relative z-10 p-4">
+          <EndlessMatch />
+        </div>
+      </div>
     </SoundProvider>
   )
 }
